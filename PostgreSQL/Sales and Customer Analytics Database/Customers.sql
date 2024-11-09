260c444af19b7c57771448259a59233660bd74b5
vcovelli <vincent.covelli78@gmail.com>
2024-11-08 21:06:17 -0500

    Added stores table SQL

--- PostgreSQL/Sales and Customer Analytics Database/Customers.sql
+++ PostgreSQL/Sales and Customer Analytics Database/Customers.sql
@@ -1,4 +1,4 @@
-CREATE TABLE (
+CREATE TABLE customers (
     customer_id SERIAL PRIMARY KEY,
     name VARCHAR(100)
 );
---
+++ PostgreSQL/Sales and Customer Analytics Database/Stores.sql
@@ -0,0 +1,5 @@
+CREATE TABLE stores (
+    store_id SERIAL PRIMARY KEY,
+    location VARCHAR(100), 
+    manager VARCHAR(100)
+);
